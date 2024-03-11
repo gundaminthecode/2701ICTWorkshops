f766c7f3829a2b1544570cc95d02074b2444fa59
gundaminthecode <nmath2211@outlook.com>
2024-03-12 09:53:44 +1000

    Added Tour-of Heroes - Week 2

---
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+import {
+  /* . . . */
+  NgFor,
+  /* . . . */
+} from '@angular/common';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
