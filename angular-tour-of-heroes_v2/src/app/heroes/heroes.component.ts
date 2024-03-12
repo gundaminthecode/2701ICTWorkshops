35e7243d841a50919e46b96631ffaea3df9fa908
gundaminthecode <nmath2211@outlook.com>
2024-03-12 14:12:12 +1000

    Tour of Heroes, no module.ts

---
+++ angular-tour-of-heroes_v2/src/app/heroes/heroes.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { NgIf, UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heroes';
+
+import {
+  /* . . . */
+  NgFor,
+  /* . . . */
+} from '@angular/common';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [UpperCasePipe, FormsModule, NgFor, NgIf],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
+
+
