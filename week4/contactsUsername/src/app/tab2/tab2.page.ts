d2b54def387be442a3e9284704f9a60300f37fc3
gundaminthecode <nmath2211@outlook.com>
2024-03-28 11:02:52 +1000

    NgModel data binding working

--- week4/contactsUsername/src/app/tab2/tab2.page.ts
+++ week4/contactsUsername/src/app/tab2/tab2.page.ts
@@ -1,20 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonList, IonIcon, IonCheckbox, IonItem, IonInput } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { addIcons } from 'ionicons';
 import { logInOutline } from 'ionicons/icons';
 import { RouterLink } from '@angular/router';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonButton, IonList, IonIcon, IonCheckbox, IonItem, IonInput, RouterLink]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonButton, IonList, IonIcon, IonCheckbox, IonItem, IonInput, RouterLink, FormsModule]
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
+  username = '';
   Count: number = 0;
 
   constructor( private router: Router) {
@@ -28,6 +30,10 @@ export class Tab2Page {
   login() {
     this.incrementCounter();
     this.router.navigateByUrl('/account');
+  }
+
+  ngOnInit(): void {
+    
   }
 
 }
