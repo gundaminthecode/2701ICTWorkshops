a7f690ef25811a17baba8a55c9fd5f94b70d03bc
gundaminthecode <nmath2211@outlook.com>
2024-04-15 21:38:53 +1000

    Created add-contact-modal

--- week6/contacts/src/app/tab1/tab1.page.ts
+++ week6/contacts/src/app/tab1/tab1.page.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonList, IonIcon, IonCheckbox, IonItem, IonInput } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule } from '@angular/common';
+import { addIcons } from 'ionicons';
+import { add } from 'ionicons/icons';
+import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+
+class Contact {
+  constructor(public firstName: string, public lastName: string, public email: string) {}
+}
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonList, IonIcon, IonCheckbox, IonItem, IonInput, ExploreContainerComponent, CommonModule, RouterLink],
 })
+
 export class Tab1Page {
-  constructor() {}
+
+  contacts: Contact[] = [
+    new Contact('Fran', 'Jipani', 'f.jipani@griffith.edu.au'),
+    new Contact('John', 'Smith', 'j.smith@griffith.edu.au')
+  ];
+
+  addContact() {
+  
+  }
+
+  constructor( private router: Router) {
+    addIcons({ add });
+  }
 }
