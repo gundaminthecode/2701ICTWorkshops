ea020f43aa32cdadd247be624da5d2e095e7d47c
gundaminthecode <nmath2211@outlook.com>
2024-04-16 16:18:43 +1000

    Edit Contacts

--- week6/contactsV2/src/app/contact-modal/contact-modal.component.ts
+++ week6/contactsV2/src/app/contact-modal/contact-modal.component.ts
@@ -1,33 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonButton, IonButtons, IonIcon, IonInput} from '@ionic/angular/standalone';
+// Ionic standalone components
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonButton, IonButtons, IonIcon, IonInput, IonItemSliding} from '@ionic/angular/standalone';
 
+import { Contact } from '../tab1/tab1.page'; // Import the Contact model
 
 @Component({
   selector: 'app-contact-modal',
   templateUrl: './contact-modal.component.html',
   styleUrls: ['./contact-modal.component.scss'],
-  standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonButton, IonIcon, IonInput, IonButtons,
-            FormsModule]
+  standalone: true, // Indicates that this component can be used independently
+  imports: [
+    // Import necessary Ionic components and FormsModule
+    IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonButton, IonIcon, IonInput, IonButtons,
+    FormsModule
+  ]
 })
 export class ContactModalComponent  implements OnInit {
 
-  firstName: string = "";
-  lastName: string = "";
-  email: string = "";
+  @Input() mode: 'add' | 'edit' = 'add'; // Input property for mode ('add' or 'edit')
+  @Input() contact: Contact | null = null; // Input property for contact data
+
+  firstName: string = ""; // Initialize firstName
+  lastName: string = ""; // Initialize lastName
+  email: string = ""; // Initialize email
+
+  modeName: string = "Add"; // Initialize modeName
 
   constructor(private modalController: ModalController) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    // If mode is 'edit' and contact is provided, set modeName to 'Update' and initialize form fields with contact data
+    if (this.mode === "edit" && this.contact) {
+      this.modeName = "Update";
+      this.firstName = this.contact.firstName;
+      this.lastName = this.contact.lastName;
+      this.email = this.contact.email;
+    }
+  }
 
   dismiss() {
-    this.modalController.dismiss();
+    this.modalController.dismiss(); // Dismiss the modal
   }
 
   addContact() {
+    // Create a contact object with input values and dismiss the modal with contact data
     const contact = {
       firstName: this.firstName,
       lastName: this.lastName,
@@ -35,4 +54,5 @@ export class ContactModalComponent  implements OnInit {
     };
     this.modalController.dismiss(contact);
   }
+
 }
--- week6/contactsV2/src/app/tab1/tab1.page.ts
+++ week6/contactsV2/src/app/tab1/tab1.page.ts
@@ -11,6 +11,7 @@ import { add } from 'ionicons/icons';
 import { ModalController } from '@ionic/angular';
 import { ContactModalComponent } from '../contact-modal/contact-modal.component';
 
+// Define a Contact class to represent contact information
 export class Contact {
   constructor(public firstName: string, public lastName: string, public email: string) {}
 }
@@ -19,29 +20,38 @@ export class Contact {
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonList, IonButton, IonIcon, IonItemSliding, IonItemOption, IonItemOptions,
-    ExploreContainerComponent, CommonModule],
-  providers: [ModalController]
+  standalone: true, // Indicates that this component can be used independently
+  imports: [
+    // Import necessary Ionic components and CommonModule
+    IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonList, IonButton, IonIcon, IonItemSliding, IonItemOption, IonItemOptions,
+    ExploreContainerComponent, CommonModule
+  ],
+  providers: [ModalController] // Declare ModalController as a provider for dependency injection
 })
 
 export class Tab1Page {
   
+  // Array to store contacts
   contacts: Contact[] = [
     new Contact('Fran', 'Jipani', 'f.jipani@griffith.edu.au'),
     new Contact('John', 'Smith', 'j.smith@griffith.edu.au')
   ];
 
   constructor(private modalController: ModalController) {
+    // Add icons to the IonIcon library
     addIcons({add});
   }
 
+  // Method to open the contact modal for adding a new contact
   async openContactModal(){
-    console.log("hi")
     const modal = await this.modalController.create({
-      component: ContactModalComponent
+      component: ContactModalComponent,
+      componentProps: {
+        mode: 'add', // Set the mode to 'add' to indicate adding a new contact
+      }
     });
 
+    // Handle the data returned when the modal is dismissed
     modal.onDidDismiss().then(data => {
       if (data && data.data) {
         const contactData = data.data;
@@ -52,14 +62,49 @@ export class Tab1Page {
     return await modal.present();
   }
 
+  // Method to open the contact modal for editing an existing contact
+  async openContactModalEdit(contact: Contact){
+    const modal = await this.modalController.create({
+      component: ContactModalComponent,
+      componentProps: {
+        mode: 'edit', // Set the mode to 'edit' to indicate editing an existing contact
+        contact: contact, // Pass the contact to be edited to the modal
+      }
+    });
+
+    // Handle the data returned when the modal is dismissed
+    modal.onDidDismiss().then((data) => {
+      if (data && data.data) {
+        const contactData = data.data;
+        this.saveContact(contact, contactData); // Call saveContact with edited data
+      }
+    });
+
+    return await modal.present();
+  }
+
+  // Method to add a new contact to the contacts array
   addContact(contactData: any){
     if (contactData){
       const {firstName, lastName, email} = contactData;
-      const newContact = new Contact (firstName, lastName, email);
-      this.contacts.push(newContact)
+      const newContact = new Contact(firstName, lastName, email);
+      this.contacts.push(newContact);
     }
   }
 
+  // Method to save the edited contact data
+  saveContact(originalContact: Contact, editedContactData: {firstName: string, lastName: string, email: string}) {
+    // Find the index of the original contact in the array
+    const index = this.contacts.indexOf(originalContact);
+    if (index !== -1) {
+      // Update the contact with edited data
+      this.contacts[index].firstName = editedContactData.firstName;
+      this.contacts[index].lastName = editedContactData.lastName;
+      this.contacts[index].email = editedContactData.email;
+    }
+  }
+
+  // Method to delete a contact from the contacts array
   deleteContact(contact: Contact){
     const index = this.contacts.indexOf(contact);
     if (index !== -1) {
