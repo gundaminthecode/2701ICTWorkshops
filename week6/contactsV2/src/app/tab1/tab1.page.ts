2c17f4d348311d03bc9fb84a8f007fb4f2401c79
gundaminthecode <nmath2211@outlook.com>
2024-04-16 11:59:37 +1000

    Display List of Contacts

--- week6/contactsV2/src/app/tab1/tab1.page.ts
+++ week6/contactsV2/src/app/tab1/tab1.page.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule } from '@angular/common';
+
+export class Contact {
+  constructor(public firstName: string, public lastName: string, public email: string) {}
+}
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonList, ExploreContainerComponent, CommonModule],
 })
+
 export class Tab1Page {
+  
+  contacts: Contact[] = [
+    new Contact('Fran', 'Jipani', 'f.jipani@griffith.edu.au'),
+    new Contact('John', 'Smith', 'j.smith@griffith.edu.au')
+  ];
+
   constructor() {}
 }
