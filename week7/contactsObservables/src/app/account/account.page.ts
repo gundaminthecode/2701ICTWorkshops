355601553de5156887ff8432dc12e8109dfe722a
gundaminthecode <nmath2211@outlook.com>
2024-04-22 13:49:04 +1000

    Routes with observables

--- week7/contactsObservables/src/app/account/account.page.ts
+++ week7/contactsObservables/src/app/account/account.page.ts
@@ -1,8 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { FormsModule } from '@angular/forms';
+// import { IonicModule } from '@ionic/angular';
+// import { ActivatedRoute } from '@angular/router';
+
+// import { Observable } from 'rxjs';
+// import { map } from 'rxjs';
+
+// @Component({
+//   selector: 'app-account',
+//   templateUrl: './account.page.html',
+//   styleUrls: ['./account.page.scss'],
+//   standalone: true,
+//   imports: [IonicModule, CommonModule, FormsModule]
+// })
+// export class AccountPage implements OnInit {
+
+//   //username: string = '';
+//   username: Observable<string> | undefined;
+
+//   constructor(private route: ActivatedRoute) { }
+
+//   ngOnInit() {
+//     //this.username = this.route.snapshot.paramMap.get('username') ?? "";
+//     this.username = this.route.paramMap.pipe(
+//       map(paramMap => paramMap.get('username') ?? "")
+//     )
+//   }
+
+// }
+
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-account',
@@ -11,14 +45,29 @@ import { ActivatedRoute } from '@angular/router';
   standalone: true,
   imports: [IonicModule, CommonModule, FormsModule]
 })
-export class AccountPage implements OnInit {
+export class AccountPage implements OnInit, OnDestroy {
 
-  username: string = '';
+  usernameSubject: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  username$: Observable<string> = this.usernameSubject.asObservable();
+  routeParamsSubscription: Subscription | undefined;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.username = this.route.snapshot.paramMap.get('username') ?? "";
+    // Subscribe to route params changes
+    this.routeParamsSubscription = this.route.params.subscribe((params: Params) => {
+      // Retrieve the username from route params
+      const username = params['username'] ?? "";
+      // Update the username subject
+      this.usernameSubject.next(username);
+    });
+  }
+
+  ngOnDestroy() {
+    // Unsubscribe from route params changes to avoid memory leaks
+    if (this.routeParamsSubscription) {
+      this.routeParamsSubscription.unsubscribe();
+    }
   }
 
 }
