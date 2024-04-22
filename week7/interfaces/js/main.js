77fcb77130f2927b392e285bdd77b7a233e7c504
gundaminthecode <nmath2211@outlook.com>
2024-04-22 14:07:12 +1000

    Create Movie Class

--- week7/interfaces/js/main.js
+++ week7/interfaces/js/main.js
@@ -9,5 +9,18 @@ var Book = /** @class */ (function () {
     };
     return Book;
 }());
-var book = new Book(1776, "Adam Smith", "The Nature and Cause of the Wealth of Nations");
-book.display();
+var Movie = /** @class */ (function () {
+    function Movie(year, director, title) {
+        this.year = year;
+        this.director = director;
+        this.title = title;
+    }
+    Movie.prototype.display = function () {
+        console.log("".concat(this.year, ", ").concat(this.director, ", \"").concat(this.title, "\""));
+    };
+    return Movie;
+}());
+var bookItem = new Book(1776, "Adam Smith", "The Nature and Cause of the Wealth of Nations");
+bookItem.display();
+var movieItem = new Movie(2007, "Michael Bay", "Transformers");
+movieItem.display();
--- week7/interfaces/src/main.ts
+++ week7/interfaces/src/main.ts
@@ -1,2 +1,5 @@
-let book: Item = new Book(1776, "Adam Smith", "The Nature and Cause of the Wealth of Nations");
-book.display();
+let bookItem: Item = new Book(1776, "Adam Smith", "The Nature and Cause of the Wealth of Nations");
+bookItem.display();
+
+let movieItem: Item = new Movie(2007, "Michael Bay", "Transformers");
+movieItem.display();
