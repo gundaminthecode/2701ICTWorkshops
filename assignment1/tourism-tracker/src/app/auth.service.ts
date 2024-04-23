05f2b77d9a1c7acb40668c83983b256897d6a69a
gundaminthecode <nmath2211@outlook.com>
2024-04-23 16:36:23 +1000

    login page with login auth? logs out upon refresh :)

--- assignment1/tourism-tracker/src/app/auth.service.ts
+++ assignment1/tourism-tracker/src/app/auth.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-  isLoggedIn: boolean = true;
+  isLoggedIn: boolean = false;
 
   constructor() {}
 }
--- assignment1/tourism-tracker/src/app/login/login.page.ts
+++ assignment1/tourism-tracker/src/app/login/login.page.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { 
+  IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonList, IonIcon, IonCheckbox, IonItem, IonInput 
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { logInOutline } from 'ionicons/icons';
+import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +16,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  username: string = '';
+  Count: number = 0;
 
-  ngOnInit() {
+  constructor(private router: Router, private authservice: AuthService) {
+    addIcons({ logInOutline });
   }
 
+  incrementCounter(){
+    this.Count += 1;
+  }
+
+  login() {
+    this.incrementCounter();
+    console.log(this.authservice.isLoggedIn);
+    this.authservice.isLoggedIn = true;
+    console.log(this.authservice.isLoggedIn);
+    console.log(this.username);
+    this.router.navigate(['']);
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
+
