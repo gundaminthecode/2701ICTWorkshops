f0443eb28029350938cc0a2aa25807a6f43c87ee
gundaminthecode <nmath2211@outlook.com>
2024-05-14 09:55:50 +1000

    Week 9 Task 2

--- week9/mapsAndCharts/src/app/map/map.page.ts
+++ week9/mapsAndCharts/src/app/map/map.page.ts
@@ -17,6 +17,8 @@ export class MapPage implements OnInit {
 
   constructor(private readonly elementRef: ElementRef) { 
     this.mapElement = elementRef;
+
+    
   }
 
   @ViewChild('map', { static: false }) mapElement: ElementRef;
@@ -25,7 +27,7 @@ export class MapPage implements OnInit {
   ngOnInit() {
     console.log('ngOnInit MapPage');
 
-    let latLng = new google.maps.LatLng(-34.9290, 138.6010);
+    let latLng = new google.maps.LatLng(-27.5522951875278, 153.05107960000726);
     let mapOptions = {
       center: latLng,
       zoom: 15,
@@ -33,6 +35,22 @@ export class MapPage implements OnInit {
     }
 
     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+      });
+
+      
+      let infoWindow = new google.maps.InfoWindow({
+        content: '<h4>Nathan Campus</h4>'
+        });
+        google.maps.event.addListener(marker, 'click', () => {
+        infoWindow.open(this.map, marker);
+        });
   }
+
+  
 
 }
