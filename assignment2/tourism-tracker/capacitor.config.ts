c02fdcd463d9f005799344e508eb1bc54b7c61ab
gundaminthecode <nmath2211@outlook.com>
2024-05-23 11:10:34 +1000

    appid and appname updated

--- assignment2/tourism-tracker/capacitor.config.ts
+++ assignment2/tourism-tracker/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'au.com.domain.tourismtracker',
-  appName: 'tourismtracker',
+  appName: 'Tourism Tracker',
   webDir: 'www'
 };
 
