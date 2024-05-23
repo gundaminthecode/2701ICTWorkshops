88556a54710d5df4ea4e31a4037fcf6cdde63186
gundaminthecode <nmath2211@outlook.com>
2024-05-23 11:09:59 +1000

    appid and appname updated

--- assignment2/tourism-tracker/capacitor.config.ts
+++ assignment2/tourism-tracker/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'tourism-tracker-v2',
+  appId: 'au.com.domain.tourismtracker',
+  appName: 'tourismtracker',
   webDir: 'www'
 };
 
